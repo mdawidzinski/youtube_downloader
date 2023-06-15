e859b3729a99bf57adf1a216ffe96daed792b17f
Tomasz Trzciński <trzcinski.tomasz.1988@gmail.com>
2023-06-15 11:50:03 +0200

    Created versions file
    Added versions file to utilities as legend of the module.

---
+++ utils/__versions__.py
@@ -0,0 +1,16 @@
+#   __  __          _____  ______ _  __  _    _ _______ _____ _       _____
+#  |  \/  |   /\   |  __ \|  ____| |/ / | |  | |__   __|_   _| |     / ____|
+#  | \  / |  /  \  | |__) | |__  | ' /  | |  | |  | |    | | | |    | (___
+#  | |\/| | / /\ \ |  _  /|  __| |  <   | |  | |  | |    | | | |     \___ \
+#  | |  | |/ ____ \| | \ \| |____| . \  | |__| |  | |   _| |_| |____ ____) |
+#  |_|  |_/_/    \_\_|  \_\______|_|\_\  \____/   |_|  |_____|______|_____/
+#
+
+__title__ = 'marek_utils'
+__description__ = 'Python utils created from externalized functions.'
+__url__ = 'https://github.com/mdawidzinski/youtube_downloader/tree/develop/utils'
+__version__ = '1.0.0'
+__author__ = 'Marek Dawidziński'
+__author_email__ = 'https://www.linkedin.com/in/marek-dawidzinski-/'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2023 Marek Dawidziński'
